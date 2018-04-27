4aa71058eb880eff9a9a7c824f7e7fc47b6c008f
Mohnish G J <mail4mohnish@gmail.com>
2018-04-27 07:50:50 +0530

    Initial commit

---
+++ lib/sales_tax.ex
@@ -0,0 +1,18 @@
+defmodule SalesTax do
+  @moduledoc """
+  Documentation for SalesTax.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SalesTax.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
