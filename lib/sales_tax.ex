7338c72cae2b1589bc8fea2b6ca15c5c2cc3a26f
Mohnish G J <mail4mohnish@gmail.com>
2018-05-18 12:56:36 +0530

    Refactor code using extract method refactoring
    Please note tl is used to get you the tail of a list.

--- lib/sales_tax.ex
+++ lib/sales_tax.ex
@@ -9,50 +9,62 @@ defmodule SalesTax do
   ## Examples
 
       iex> SalesTax.compute
-[
-  %Item{
-    basic_sales_tax_applicable: false,
-    imported: false,
-    name: "book",
-    price: 12.49,
-    quantity: "1"
-  },
-  %Item{
-    basic_sales_tax_applicable: true,
-    imported: false,
-    name: "music cd",
-    price: 16.489,
-    quantity: "1"
-  },
-  %Item{
-    basic_sales_tax_applicable: false,
-    imported: false,
-    name: "chocolate bar",
-    price: 0.85,
-    quantity: "1"
-  }
-]
+%ShoppingCart{
+  items: [
+    %Item{
+      basic_sales_tax_applicable: false,
+      imported: false,
+      name: "book",
+      price: 12.49,
+      quantity: 1
+    },
+    %Item{
+      basic_sales_tax_applicable: true,
+      imported: false,
+      name: "music cd",
+      price: 16.49,
+      quantity: 1
+    },
+    %Item{
+      basic_sales_tax_applicable: false,
+      imported: false,
+      name: "chocolate bar",
+      price: 0.85,
+      quantity: 1
+    }
+  ],
+  sales_tax: 1.5,
+  total: 29.83
+}
 
   """
   def compute do
-    rows =
       File.stream!("input/shopping_basket1.csv")
       |> Enum.to_list
+      |> tl
+      |> get_products()
+      |> populate_shopping_cart_items()
+  end
 
-    [_ | items] = rows
+  defp get_products(items) do
+    Enum.map items, fn item ->
+       parse_item(item)
+       |> imported?()
+       |> basic_sales_tax_applicable?()
+    end
+  end
 
-    products = Enum.map items, fn item ->
-                 product = item |> String.split(",")
-                 %Item{quantity: parse_quantity(product), name: parse_name(product), price: parse_price(product)}
-                 |> imported?()
-                 |> basic_sales_tax_applicable?()
-               end
-
+  defp populate_shopping_cart_items(products) do
     List.foldl(products, ShoppingCart.new, fn product, shopping_cart ->
       total_sales_tax_from_one_item = SalesTaxCalculator.calculate_total_sales_tax(product)
       cart_product = initialize_cart_product(product, total_sales_tax_from_one_item)
       update_shopping_cart(shopping_cart, cart_product, total_sales_tax_from_one_item)
     end)
+  end
+
+  defp parse_item(item) do
+    product = item |> String.split(",")
+    %Item{quantity: parse_quantity(product), name: parse_name(product), price: parse_price(product)}
   end
 
   defp parse_quantity(product) do
