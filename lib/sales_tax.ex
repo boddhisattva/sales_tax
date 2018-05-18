e9c8b9467015591799be076fc4687e3fe810eacc
Mohnish G J <mail4mohnish@gmail.com>
2018-05-18 11:56:14 +0530

    Refactor code that updates a shopping cart details

--- lib/sales_tax.ex
+++ lib/sales_tax.ex
@@ -51,10 +51,7 @@ defmodule SalesTax do
     items_with_tax =  List.foldl(products, ShoppingCart.new, fn product, shopping_cart ->
                       total_sales_tax_from_one_item = SalesTaxCalculator.calculate_total_sales_tax(product)
                       cart_product = initialize_cart_product(product, total_sales_tax_from_one_item)
-                      %{ shopping_cart
-                         | total: shopping_cart.total + Float.round(cart_product.price, 2),
-                           sales_tax: shopping_cart.sales_tax + total_sales_tax_from_one_item * product.quantity,
-                           items: shopping_cart.items ++ [cart_product] }
+                      update_shopping_cart(shopping_cart, cart_product, total_sales_tax_from_one_item)
                     end)
   end
 
@@ -78,6 +75,13 @@ defmodule SalesTax do
     %Item{price: (Float.round(product.price + total_sales_tax_from_one_item, 2)) * product.quantity, quantity: product.quantity,
           name: product.name, basic_sales_tax_applicable: product.basic_sales_tax_applicable,
           imported: product.imported}
+  end
+
+  defp update_shopping_cart(shopping_cart, cart_product, total_sales_tax_from_one_item) do
+    %{ shopping_cart
+       | total: shopping_cart.total + Float.round(cart_product.price, 2),
+         sales_tax: shopping_cart.sales_tax + total_sales_tax_from_one_item * cart_product.quantity,
+         items: shopping_cart.items ++ [cart_product] }
   end
 
   defp imported?(item) do
