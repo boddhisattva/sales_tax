fd0f148e94c305889b399081a94ca7241ea4aa54
Mohnish G J <mail4mohnish@gmail.com>
2018-11-05 14:57:24 +0530

    Add receipt generator related tests

---
+++ test/receipt_generator_test.ex
@@ -0,0 +1,49 @@
+defmodule ReceiptGeneratorTest do
+  use ExUnit.Case
+
+  import ExUnit.CaptureIO
+
+  describe "given cart details" do
+    test "it prints out the cart details" do
+      cart_details = %ShoppingCart{
+                        items: [
+                          %Item{
+                            basic_sales_tax_applicable: false,
+                            imported: false,
+                            name: "book",
+                            price: 12.49,
+                            quantity: 1
+                          },
+                          %Item{
+                            basic_sales_tax_applicable: true,
+                            imported: false,
+                            name: "music cd",
+                            price: 16.49,
+                            quantity: 1
+                          },
+                          %Item{
+                            basic_sales_tax_applicable: false,
+                            imported: false,
+                            name: "chocolate bar",
+                            price: 0.85,
+                            quantity: 1
+                          }
+                        ],
+                        sales_tax: 1.5,
+                        total: 29.83
+                      }
+
+    receipt_details = fn ->
+      ReceiptGenerator.generate_details(cart_details)
+    end
+
+
+      assert capture_io(receipt_details) == "1, book, 12.49
+1, music cd, 16.49
+1, chocolate bar, 0.85
+
+Sales Taxes: 1.5
+Total: 29.83\n"
+    end
+  end
+end
